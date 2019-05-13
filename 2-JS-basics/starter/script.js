b95e0985bd8d709420307cd71feb5fb64e8110ad
github username <github email address>
2019-05-13 13:22:48 -0500

    logical operator

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -20,37 +20,67 @@
 
 /*Variables mutation and type coercion */
 
-var firstName = "John";
-var age = 28;
+// var firstName = "John";
+// var age = 28;
 
-// Type coercion
-console.log(firstName + " " + age);
+// // Type coercion
+// console.log(firstName + " " + age);
 
-var job, isMarried;
-job = "teacher";
-isMarried = false;
+// var job, isMarried;
+// job = "teacher";
+// isMarried = false;
 
-console.log(
-  firstName +
-    " is a " +
-    age +
-    " year old " +
-    job +
-    ". Is he married? " +
-    isMarried
-);
+// console.log(
+//   firstName +
+//     " is a " +
+//     age +
+//     " year old " +
+//     job +
+//     ". Is he married? " +
+//     isMarried
+// );
 
-// Variable mutation
-age = "twenty eight";
-job = "driver";
-alert(
-  firstName +
-    " is a " +
-    age +
-    " year old " +
-    job +
-    ". Is he married? " +
-    isMarried
-);
-var lastName = prompt("What is his last name?");
-console.log(firstName + " " + lastName);
+// // Variable mutation
+// age = "twenty eight";
+// job = "driver";
+// alert(
+//   firstName +
+//     " is a " +
+//     age +
+//     " year old " +
+//     job +
+//     ". Is he married? " +
+//     isMarried
+// );
+// var lastName = prompt("What is his last name?");
+// console.log(firstName + " " + lastName);
+
+/**************
+ * Basic operators
+ */
+var now, yearJohn, yearMark;
+now = 2018;
+ageJohn = 28;
+ageMark = 33;
+
+// Math operators
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+
+console.log(yearJohn);
+console.log(yearMark);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+// // Logical operators
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
+
+// // typeof operator
+// console.log(typeof johnOlder);
+// console.log(typeof ageJohn);
+// console.log(typeof 'Mark is older tha John');
+// var x;
+// console.log(typeof x);
