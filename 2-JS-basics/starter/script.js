4c56bf3346f2780c885c0dafafa299cd64eae16b
github username <github email address>
2019-05-13 16:18:50 -0500

    conditional statements

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -122,18 +122,52 @@
 //  mass / (height * height);
 //  mass in kg and height in meters
 
-let jMass = 80;
-let jHeight = 4;
-let mMass = 60;
-let mHeight = 5;
+// let jMass = 80;
+// let jHeight = 4;
+// let mMass = 60;
+// let mHeight = 5;
 
-var jBMI = (jMass / jHeight) ^ 2;
-console.log(jBMI);
+// var jBMI = (jMass / jHeight) ^ 2;
+// console.log(jBMI);
 
-var mBMI = (mMass / mHeight) ^ 2;
-console.log(mBMI);
+// var mBMI = (mMass / mHeight) ^ 2;
+// console.log(mBMI);
 
-var isHigher = jBMI >= mBMI;
-// console.log(isHigher);
+// var isHigher = jBMI > mBMI;
+// // console.log(isHigher);
 
-console.log("is john's' BMI higher than + mark's? " + isHigher);
+// console.log("is john's' BMI higher than + mark's? " + isHigher);
+/******
+ * If / else statement
+ */
+
+var firstName = "John";
+var civilStatus = "single";
+
+if (civilStatus === "married") {
+  console.log(firstName + " is married!");
+} else {
+  console.log(firstName + " will hopefully marry soon :)");
+}
+
+var isMarried = true;
+if (isMarried) {
+  console.log(firstName + " is married!");
+} else {
+  console.log(firstName + " will hopefully marry soon :)");
+}
+
+var massMark = 78; // kg
+var heightMark = 1.69; // meters
+
+var massJohn = 92;
+var heightJohn = 1.95;
+
+var BMIMark = massMark / (heightMark * heightMark);
+var BMIJohn = massJohn / (heightJohn * heightJohn);
+
+if (BMIMark > BMIJohn) {
+  console.log("Mark's BMI is higher than John's.");
+} else {
+  console.log("John's BMI is higher than Marks's.");
+}
