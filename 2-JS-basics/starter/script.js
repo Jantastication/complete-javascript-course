5084f77c5a4da559b855f2626842f49f21e81891
github username <github email address>
2019-05-13 16:28:58 -0500

    Boolean

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -141,33 +141,49 @@
  * If / else statement
  */
 
+// var firstName = "John";
+// var civilStatus = "single";
+
+// if (civilStatus === "married") {
+//   console.log(firstName + " is married!");
+// } else {
+//   console.log(firstName + " will hopefully marry soon :)");
+// }
+
+// var isMarried = true;
+// if (isMarried) {
+//   console.log(firstName + " is married!");
+// } else {
+//   console.log(firstName + " will hopefully marry soon :)");
+// }
+
+// var massMark = 78; // kg
+// var heightMark = 1.69; // meters
+
+// var massJohn = 92;
+// var heightJohn = 1.95;
+
+// var BMIMark = massMark / (heightMark * heightMark);
+// var BMIJohn = massJohn / (heightJohn * heightJohn);
+
+// if (BMIMark > BMIJohn) {
+//   console.log("Mark's BMI is higher than John's.");
+// } else {
+//   console.log("John's BMI is higher than Marks's.");
+// }
+
+/***********
+ * Boolean Logic
+ */
 var firstName = "John";
-var civilStatus = "single";
+var age = 20;
 
-if (civilStatus === "married") {
-  console.log(firstName + " is married!");
+if (age < 13) {
+  console.log(firstName + " is a boy.");
+} else if (age >= 13 && age < 20) {
+  console.log(firstName + " is a teenager.");
+} else if (age >= 20 && age < 30) {
+  console.log(firstName + " is a young man.");
 } else {
-  console.log(firstName + " will hopefully marry soon :)");
-}
-
-var isMarried = true;
-if (isMarried) {
-  console.log(firstName + " is married!");
-} else {
-  console.log(firstName + " will hopefully marry soon :)");
-}
-
-var massMark = 78; // kg
-var heightMark = 1.69; // meters
-
-var massJohn = 92;
-var heightJohn = 1.95;
-
-var BMIMark = massMark / (heightMark * heightMark);
-var BMIJohn = massJohn / (heightJohn * heightJohn);
-
-if (BMIMark > BMIJohn) {
-  console.log("Mark's BMI is higher than John's.");
-} else {
-  console.log("John's BMI is higher than Marks's.");
+  console.log(firstName + " is a man.");
 }
