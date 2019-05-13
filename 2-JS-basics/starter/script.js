b4d3b3f58e52c222e2c21ecfde6916b4cac57a5b
github username <github email address>
2019-05-13 12:56:43 -0500

    type coercion

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,1 +1,43 @@
-console.log("hello hi");
+// // Variables and data types
+// let firstName = "John";
+// console.log(firstName);
+
+// const lastName = "Smith";
+// const age = 28;
+
+// const fullAge = true;
+// console.log(fullAge);
+
+// let job;
+// console.log(job);
+
+// job = "Teacher";
+// console.log(job);
+
+// // Variable naming rules
+// let _3years = 3
+// var johnMark = "John and Mark"
+
+/*Variables mutation and type coercion */
+
+var firstName = "John";
+var age = 28;
+
+// Type coercion
+console.log(firstName + " " + age);
+
+var job, isMarried;
+job = "teacher";
+isMarried = false;
+
+console.log(
+  firstName +
+    " is a " +
+    age +
+    " year old " +
+    job +
+    ". Is he married? " +
+    isMarried
+);
+
+// Variable mutation
