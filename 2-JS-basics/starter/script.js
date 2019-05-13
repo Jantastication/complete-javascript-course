86c36d2b7ccec4d8ce15c55ca340731dffd52a18
github username <github email address>
2019-05-13 13:03:51 -0500

    variable mutation

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -41,3 +41,16 @@ console.log(
 );
 
 // Variable mutation
+age = "twenty eight";
+job = "driver";
+alert(
+  firstName +
+    " is a " +
+    age +
+    " year old " +
+    job +
+    ". Is he married? " +
+    isMarried
+);
+var lastName = prompt("What is his last name?");
+console.log(firstName + " " + lastName);
