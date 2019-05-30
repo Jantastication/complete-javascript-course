30cc3fb8c01278e00eed01c4654ad169f4405d6e
github username <github email address>
2019-05-30 13:20:46 -0500

    arrays

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -351,29 +351,29 @@
  * Functions
  */
 
-function calculateAge(birthYear) {
-  return 2018 - birthYear;
-}
+// function calculateAge(birthYear) {
+//   return 2018 - birthYear;
+// }
 
-var ageJohn = calculateAge(1990);
-var ageMike = calculateAge(1986);
-var ageJane = calculateAge(1989);
-console.log(ageJohn, ageJane, ageMike);
+// var ageJohn = calculateAge(1990);
+// var ageMike = calculateAge(1986);
+// var ageJane = calculateAge(1989);
+// console.log(ageJohn, ageJane, ageMike);
 
-function yearsUntilRetirement(year, firstName) {
-  var age = calculateAge(year);
-  var retirement = 65 - age;
+// function yearsUntilRetirement(year, firstName) {
+//   var age = calculateAge(year);
+//   var retirement = 65 - age;
 
-  if (retirement > 0) {
-    console.log(firstName + " retires in " + retirement + " years.");
-  } else {
-    console.log(firstName + " is already retired.");
-  }
-}
+//   if (retirement > 0) {
+//     console.log(firstName + " retires in " + retirement + " years.");
+//   } else {
+//     console.log(firstName + " is already retired.");
+//   }
+// }
 
-yearsUntilRetirement(1990, "John");
-yearsUntilRetirement(1948, "Mike");
-yearsUntilRetirement(1969, "Jane");
+// yearsUntilRetirement(1990, "John");
+// yearsUntilRetirement(1948, "Mike");
+// yearsUntilRetirement(1969, "Jane");
 
 /*****************************
  * Function Statements and Expressions
@@ -383,19 +383,82 @@ yearsUntilRetirement(1969, "Jane");
 //function whatDoYouDo(job, firstName) {}
 
 // Function expression
-var whatDoYouDo = function(job, firstName) {
-  switch (job) {
-    case "teacher":
-      return firstName + " teaches kids how to code";
-    case "driver":
-      return firstName + " drives a cab in Lisbon.";
-    case "designer":
-      return firstName + " designs beautiful websites";
-    default:
-      return firstName + " does something else";
-  }
-};
+// var whatDoYouDo = function(job, firstName) {
+//   switch (job) {
+//     case "teacher":
+//       return firstName + " teaches kids how to code";
+//     case "driver":
+//       return firstName + " drives a cab in Lisbon.";
+//     case "designer":
+//       return firstName + " designs beautiful websites";
+//     default:
+//       return firstName + " does something else";
+//   }
+// };
 
-console.log(whatDoYouDo("teacher", "John"));
-console.log(whatDoYouDo("designer", "Jane"));
-console.log(whatDoYouDo("retired", "Mark"));
+// console.log(whatDoYouDo("teacher", "John"));
+// console.log(whatDoYouDo("designer", "Jane"));
+// console.log(whatDoYouDo("retired", "Mark"));
+
+/*****************************
+ * Arrays
+ */
+
+// Initialize new array
+var names = ["John", "Mark", "Jane"];
+var years = new Array(1990, 1969, 1948);
+
+console.log(names[2]);
+console.log(names.length);
+
+// Mutate array data
+names[1] = "Ben";
+names[names.length] = "Mary";
+console.log(names);
+
+// Different data types
+var john = ["John", "Smith", 1990, "designer", false];
+
+john.push("blue");
+john.unshift("Mr.");
+console.log(john);
+
+john.pop();
+john.pop();
+john.shift();
+console.log(john);
+
+console.log(john.indexOf(23));
+
+var isDesigner =
+  john.indexOf("designer") === -1
+    ? "John is NOT a designer"
+    : "John IS a designer";
+console.log(isDesigner);
+
+/********
+ * Code Challenge
+ */
+
+function tipCalculator(bill) {
+  let percentage;
+  if (bill < 50) {
+    percentage = 0.2;
+  } else if (bill >= 50 && bill < 200) {
+    percentage = 0.15;
+  } else {
+    percentage = 0.1;
+  }
+  return percentage * bill;
+}
+console.log(tipCalculator(300));
+const bills = [124, 48, 268];
+const tips = [
+  tipCalculator(bills[0]),
+  tipCalculator(bills[1]),
+  tipCalculator(bills[2])
+];
+
+var finalValues = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(tips, finalValues);
